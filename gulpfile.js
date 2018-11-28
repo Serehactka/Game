d648c23b747b85dc8fb3ea1fbe6d6884f9296ae3
Serehactka <serehactka@gmail.com>
2018-11-28 22:32:00 +0300

    Core filled

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const babel = require('babelify');
 const uglify = require('uglifyify');
+const bs = require('browser-sync');
+const plumber = require('gulp-plumber');
+const newer = require('gulp-newer');
 
 gp.task('bundle', () => {
     browserify('assets/main.ts')
@@ -23,22 +26,37 @@ gp.task('bundle', () => {
 });
 
 gp.task('stylus', () => {
-    gp.src('assets/components/stylus/common.styl')
+    gp.src('assets/mixins/stylus/common.styl')
+        .pipe(plumber())
         .pipe(stylus())
         .pipe(gp.dest('public/css'));
 });
 
 gp.task('pug', () => {
     gp.src('assets/views/**/*.pug')
+        .pipe(plumber())
         .pipe(pug())
         .pipe(gp.dest('public'))
 });
+
+gp.task('images', () => {
+    gp.src('assets/images/**/*.+(svg|png|jpg|bmp)')
+        .pipe(newer('public/img'))
+        .pipe(gp.dest('public/img'));
+})
 
 gp.task('watch', () => {
     gp.watch(['assets/components/**/*.styl', 'assets/mixins/stylus/**/*.styl'], ['stylus']);
     gp.watch(['assets/components/**/*.pug', 'assets/mixins/pug/**/*.pug', 'assets/views/**/*.pug'], ['pug']);
     gp.watch(['assets/modules/**/*.ts', 'assets/components/**/*.ts', 'assets/*.ts'], ['bundle']);
+    gp.watch(['assets/images/**/*.+(svg|png|jpg|bmp)'], ['images']);
+});
+
+gp.task('bs', () => {
+    bs.init({
+        server: './public'
+    });
 });
 
 gp.task('build', ['bundle', 'stylus', 'pug']);
-gp.task('default', ['build', 'watch']);
+gp.task('default', ['build', 'watch', 'images', 'bs']);
